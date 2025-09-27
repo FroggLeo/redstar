901760e70ec9ffa694e02ff8c93ffc3d605103da
Leo Lin <88123479+FroggLeo@users.noreply.github.com>
2025-09-27 13:15:54 -0700

    Refactor tile collison

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 
 const SPEED = 30.0
-const JUMP_VELOCITY = -1600.0
+const JUMP_VELOCITY = -600.0
 
 
 func _physics_process(delta: float) -> void:
@@ -11,12 +11,12 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("up") and is_on_floor():
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
+	var direction := Input.get_axis("ui_left", "ui_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
