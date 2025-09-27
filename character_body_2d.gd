3d06dbf34a38d2aaa38d68904d72128774aba7cb
Leo Lin <88123479+FroggLeo@users.noreply.github.com>
2025-09-27 13:29:46 -0700

    fix movement code

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,22 +1,29 @@
 extends CharacterBody2D
 
 
-const SPEED = 30.0
-const JUMP_VELOCITY = -600.0
+const SPEED = 150.0
+const JUMP_VELOCITY = -400.0
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
+	
+	var jump = 0
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+	if Input.is_action_just_pressed("up") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jump += 1
+	elif jump == 1 and not is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		jump = 2
+	else:
+		jump = 0
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
